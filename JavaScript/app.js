2749cd31fab199fd8920ea1f3c98946be6b6c21d
ibrahim-alhamshari <ibrahimalhamshari742@gmail.com>
2021-03-28 16:11:50 +0300

    The new lab

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -1,9 +1,6 @@
 
 
-var UserName = prompt('Enter your name !');
-
-var Q = prompt('How many cars do you have ?')
-
+/*
 if (Q >= 2 ) {
    alert("In our company there are many modern cars wich work by the electric engine , Welcome to our compny ");
   document.write('<img src="https://th.bing.com/th/id/OIP.HU_m38GjUAXd72PJklQ5IAHaEc?w=301&h=181&c=7&o=5&dpr=1.25&pid=1.7" alt="Hybrid car"></img>');
@@ -20,3 +17,37 @@ if (Q >= 2 ) {
 }
 
 document.write('<h4>' + UserName + '</h4>');
+*/
+
+var UserName = prompt('Enter your name !');
+
+var carNum = prompt('How many cars do you have ?', '1, 2, 3 ');
+
+ while (carNum != '1' && carNum != '2' && carNum != '3' ){
+ carNum = prompt ('please re-enter the number of cars that do you have ! ');
+ }
+
+
+var carType = prompt (' what type of cars do you preffer :' , 'Diesel, Gasoline');
+
+
+ while( carType !=='Diesel' && carType !=='Gasoline'){
+ 
+    carType = prompt (' please retry again either Diesel or Gasoline ! ');
+ }
+var cars = 1 ;
+  cars = prompt ('How many cars do you want to show !');
+
+
+    
+for (var i=1 ; i<=cars ;i++) {
+    console.log (carType);
+if  (carType == 'Diesel')
+{
+     document.write('<img src="https://spirotours.com/wp-content/uploads/2019/08/2020-BMW-Pickup-Truck-m3for-sale-how-much-is-inside.jpg"></img>');
+ }else {
+    document.write('<img src= "https://namastecar.com/wp-content/uploads/2020/06/2021-BMW-7-Series-new-straight-six-cylinder-diesel-17.jpg"></img>');
+} }
+
+
+
